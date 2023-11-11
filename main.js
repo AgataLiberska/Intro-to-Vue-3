d9d33b3fb28cbba650458567095e2b9b10d67e1a
Agata Liberska <liberska@gmail.com>
2023-11-11 14:29:17 +0000

    lesson done

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    }
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+      console.log(this.cart)
     },
-    methods: {}
+    removeItemFromCart(id) {
+      const index = this.cart.indexOf(id)
+      if (index > -1) {
+        this.cart.splice(index, 1)
+        console.log(this.cart)
+      }
+    },
+  },
 })
